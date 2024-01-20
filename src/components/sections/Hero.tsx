47fe57e9e2587471ea0339b50289ef881920aba9
Alex <ajoshua14@icloud.com>
2024-01-19 22:57:25 -0800

    Further hero section design

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -65,15 +65,27 @@ const Hero: FC<HeroProps> = ({ }) => {
 
   return (
     <section ref={ref} className="w-full h-full flex items-center justify-center bg-black text-white">
-      <div className="absolute left-1/2 xl:left-1/4 top-1/2 -translate-y-1/2 -translate-x-1/2 xl:-translate-x-0 w-fit">
+      <div className="z-10 absolute left-1/2 xl:left-1/4 top-1/2 -translate-y-1/2 -translate-x-1/2 xl:-translate-x-0 w-fit">
         <div className="absolute top-0 left-1/2 xl:left-0 -translate-y-full -translate-x-1/2 xl:-translate-x-full text-xl">
           <h1>we create_</h1>
         </div>
         {isClient &&
-          (<p className="text-8xl whitespace-nowrap">
+          (<p className="font-hero whitespace-nowrap">
             {heroPhrase}
           </p>)
         }
+      </div>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex">
+        <div className="w-96 h-96 rounded-full bg-zinc-500 flex items-center justify-center">
+          <div className="w-11/12 aspect-square rounded-full bg-black/80 flex items-center justify-center">
+            <div className="w-3/4 aspect-square rounded-full bg-white/80 flex items-center justify-center">
+              <div className="w-1/2 aspect-square rounded-full bg-black/90">
+
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </section>
   )
