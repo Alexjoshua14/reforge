95e5ae5883efdb189ca5deb039a8b34e70f2056f
Alex <ajoshua14@icloud.com>
2024-01-22 19:56:20 -0800

    Swap out hero eye with improved version

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ interface NavBarProps {
 
 const NavBar: FC<NavBarProps> = ({ }) => {
   return (
-    <div className="z-50 fixed top-0 left-0 w-full h-40 flex justify-between items-center px-10 text-primary transition-colors font-medium">
+    <div className="z-50 fixed top-0 left-0 w-full h-40 flex justify-between items-center px-24 text-primary transition-colors font-medium">
       <h1 className="text-2xl tracking-wide">
         reforge
       </h1>
